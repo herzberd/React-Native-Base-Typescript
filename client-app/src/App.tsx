8a885dd4cd403f2eb9a7065bee7565dd8e15409b
herzberd <dphz9f@mst.edu>
2019-02-05 19:35:52 -0600

    Added files

---
+++ client-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default class App extends React.Component<{}, {}> {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return <View/>;
+  }
+}
